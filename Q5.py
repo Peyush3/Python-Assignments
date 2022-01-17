db50773bb4748bc944796ae362ffa06914927df0
Peyush3 <97232897+Peyush3@users.noreply.github.com>
2022-01-17 16:16:59 +0530

    Delete Q5.py

--- Q5.py
+++
@@ -1,7 +0,0 @@
-# Question 5
-color=['Red','Green','White','Black','Pink','Yellow']
-color.remove(color[3])
-print("Part a Answer:",color)
-color=['Red','Green','White','Black','Pink','Yellow']
-color[3:5]=['Purple']
-print("Part b Answer:",color)
